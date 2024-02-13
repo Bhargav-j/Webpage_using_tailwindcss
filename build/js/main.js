40b490041e29b82e060a81890735f95806cb549a
Bhargav-j <jeereddybhargav@gmail.com>
2024-02-13 16:34:53 +0530

    first full page

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+const initApp = () => {
+    const hanburgerBtn = document.getElementById("hamburger-button")
+    const mobileMenu = document.getElementById("mobile-menu")
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hanburgerBtn.classList.toggle('toggle-btn')
+    }
+
+    hanburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener("click", toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
